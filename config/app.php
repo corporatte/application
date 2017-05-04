1fa41f4fa9f44845c4e8d24683353bbe7b2f7460
jjsquady <jorge.techinfo@gmail.com>
2017-05-04 18:53:55 -0300

    Update default SP config

--- config/app.php
+++ config/app.php
@@ -6,7 +6,10 @@ return [
      * Return an array of providers classes
      */
     'providers' => [
-        Corporatte\Application\Providers\ApplicationServiceProvider::class,
+        /*
+         * Default application Service Providers
+         */
+        Corporatte\Core\Providers\ApplicationServiceProvider::class,
     ]
 
 ];
